67d77ec62c6a7197d0e034e5d5baf89b60ca4a43
aleahgrym <aleah.grymaloski@brentwood.ca>
2015-11-04 12:47:45 -0800

    dfsfg

---
+++ src/snakegame/SnakeGame.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snakegame;
+
+import environment.ApplicationStarter;
+
+/**
+ *
+ * @author Aleah
+ */
+public class SnakeGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ApplicationStarter.run("Snake Run", new Sky());
+        // TODO code application logic here
+    }
+    
+}
